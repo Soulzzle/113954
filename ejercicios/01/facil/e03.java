9e48a03e9c54bd2dbc89915e6643b8b978309d1d
Danel <matasdanel50@gmail.com>
2026-08-29 16:56:17 -0300

    Tercer ejercicio de java

---
+++ ejercicios/01/facil/e03.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ParOImpar {
+    /**
+     * esPar evalua si el resto de la división del número es igual a 0.
+     * @param n número a evaluar.
+     * @return true si es par, false si es impar.
+     */
+    public static boolean esPar(int n){
+        return n % 2 == 0;
+    }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresá un número: ");
+        var n = scanner.nextInt();
+        if (esPar(n)){
+            System.out.println("El número " + n + " es par.");
+        } else{
+            System.out.println("El número " + n + " es impar.");
+        }
+    }
+}
