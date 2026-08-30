bab4a7c95c6ac14cab62a96b211a7a836771992e
Danel <matasdanel50@gmail.com>
2026-08-30 18:07:11 -0300

    Ejercicio 1 medio de guía 1

---
+++ ejercicios/01/medio/e01.java
@@ -0,0 +1,28 @@
+// Máximo de un arreglo. Escribí int maximo(int[] v) y testealo con JUnit (incluí el caso de un solo elemento).
+public class MaximoArreglo {
+    
+    /**
+     * maximo devuelve el número mas alto del arreglo.
+     * @param v Arreglo tipo int[].
+     * @return maximo int del arreglo.
+     */
+    public static int maximo(int[] v){
+        if(v == null || v.length == 0){
+            return -1;
+        }
+        int maximo = v[0];
+        for(int i=1; i<v.length; i++){
+            if (v[i]>maximo){
+                maximo = v[i];
+            }
+        }
+
+        return maximo;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {3, 2, 5, 6, 12, 2};
+        int max = maximo(arr);
+        System.out.println("El máximo del arreglo es " + max);
+    }
+}
