bb9e3c418f3a2898883a61c40c4a90885045eb58
Danel <matasdanel50@gmail.com>
2026-08-29 23:40:19 -0300

    Ejercicio 6 de java

--- ejercicios/01/facil/e06.java
+++ ejercicios/01/facil/e06.java
@@ -1,4 +1,19 @@
 // Tabla de multiplicar. Con un for, imprimí la tabla del número que se pase por args.
 public class TablaMultiplicar {
     
+    /**
+     * imprimirTabla imprime por consola la tabla del número recibido.
+     * @param n El número recibido.
+     */
+    public static void imprimirTabla(int n){
+        System.out.println("La tabla de " + n + " es:");
+        for(int i=1; i<=10; i++){
+            int res = n*i;
+            System.out.println(n + "x" + i + " = " + res);
+        }
+    }
+
+    public static void main(String[] args){
+        imprimirTabla(22);
+    }
 }
