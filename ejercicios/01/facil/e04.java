fa6157d558b097d4fc4ce2f97980e9f66fbc0086
Danel <matasdanel50@gmail.com>
2026-08-29 23:24:05 -0300

    Ejercicio 4 de java

---
+++ ejercicios/01/facil/e04.java
@@ -0,0 +1,29 @@
+
+public class MayorDeDos {
+    static final int IGUALES = -1;
+    
+    /**
+     * mayor compara y devuelve el número mayor recibido por parámetro.
+     * @param a Primer número int recibido.
+     * @param b Segundo número int recibido.
+     * @return Devuelve a si a>b, b si a<b, o -1 si son iguales.
+     */
+    public static int mayor(int a, int b){
+        if(a > b){
+            return a;
+        } else if(a < b){
+            return b;
+        } else{
+            return IGUALES;
+        }
+    }
+
+    public static void main(String[] args){
+        int mayorNum = mayor(2,3);
+        if(mayorNum == IGUALES){
+            System.out.println("Ambos números son iguales.");
+        } else{
+            System.out.println("El número mayor es " + mayorNum);
+        }
+    }
+}
