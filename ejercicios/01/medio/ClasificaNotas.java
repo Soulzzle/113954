7dbd4ad4d26399e089b413a2d2d45b48b2cca772
Soulzzle <matasdanel50@gmail.com>
2026-08-31 14:51:37 -0300

    Arranco ejercicio ClasificaNotas

---
+++ ejercicios/01/medio/ClasificaNotas.java
@@ -0,0 +1,12 @@
+// Clasificador de notas. Método que reciba una nota entera y devuelva “insuficiente”/“aprobado”/“distinguido”
+// con un switch expression; lanzá una excepción propia si está fuera de 0–10.
+public class ClasificaNotas {
+    static final int NOTA_MIN = 0;
+    static final int NOTA_MAX = 10;
+    static final int NOTA_MIN_APROBADA = 4;
+    static final int NOTA_MIN_BUENA = 8;
+
+    public static String clasificar(int nota){
+
+    }
+}
