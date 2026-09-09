b34abb2720d1d3f50c8399b9b206dfe4d16fd8d1
Dan compucapital <matasdanel50@gmail.com>
2026-09-08 23:40:01 -0300

    Preparo RegistroSobreArreglo para implementar

--- tps/tp1/RegistroSobreArreglo.java
+++ tps/tp1/RegistroSobreArreglo.java
@@ -1,5 +1,28 @@
 package tp1;
 
-public class RegistroSobreArreglo {
-    // Implementación de RegistroDePrestamos interface.
+public class RegistroSobreArreglo implements RegistroDePrestamos {
+    
+    public void registrar(Prestamo p){
+
+    }
+
+    public int cantidad(){
+
+    }
+
+    public Prestamo obtener(int i){ // IndexOutOfBoundsException si i es invalido
+
+    }
+
+    public int[] padrones(){ // sin repetidos, en orden de aparicion
+
+    }
+
+    public Prestamo[] prestamosDe(int padron){ // arreglo vacio si no hay ninguno
+
+    }
+
+    public String[] titulosMasPedidos(int n){
+
+    }
 }
