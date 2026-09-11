748212c8d9b3f9ab0344f3eeec64b7b7a68be23e
Danel <matasdanel50@gmail.com>
2026-09-11 16:30:14 -0300

    ExportadorDeReporte.java terminado

--- tps/tp1/ExportadorDeReporte.java
+++ tps/tp1/ExportadorDeReporte.java
@@ -1,5 +1,8 @@
 package tp1;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 public interface ExportadorDeReporte {
     void exportar(FilaDeSocio[] filas, Path destino) throws IOException;
     String extension();   // "txt", "csv", ...
